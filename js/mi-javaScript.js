d24e5757fee27d3f9c4b8a4ce3b02d0022df84d8
EugeDev <eugegzv24@gmail.com>
2024-03-15 17:33:02 -0300

    js con jquery, exepto validaciones

--- js/mi-javaScript.js
+++ js/mi-javaScript.js
@@ -1,13 +1,37 @@
-document.addEventListener('DOMContentLoaded', inicializarBoton)
 
-function inicializarBoton() {
-    document.getElementById('btn-regis').addEventListener('click', function () {
-        manchaGrande();
-    });
-
-    document.getElementById('btn-volver').addEventListener('click', function () {
-        manchaGrande2();
+$(document).ready(function(){
+    $("#btn-regis").click(function(){
+        
+        $("#f-login").hide(500);
+        $("#f-signUp").show(500);
+         
     })
+
+    $("#btn-volver").click(function(){
+       
+        $("#f-signUp").hide(500);
+        $("#f-login").show(500);
+        $("#titulo").text('Log In');   
+    })
+});
+
+
+
+
+// ---------------------| Con JS puro|-------------------------------------------
+
+
+
+ document.addEventListener('DOMContentLoaded', inicializarBoton)
+
+ function inicializarBoton() {
+    // document.getElementById('btn-regis').addEventListener('click', function () {
+    //     manchaGrande();
+    //  });
+
+//     document.getElementById('btn-volver').addEventListener('click', function () {
+//         manchaGrande2();
+//     })
 
     document.getElementById('btn-crear').addEventListener('click', function () {
         validar();
@@ -18,102 +42,95 @@ function inicializarBoton() {
 
 // ---------------------| BOTON REGISTRARSE |-------------------------------------------
 
-function cuadroGrande()
-{
-    var cuadro = document.grtById('cuadrado');
-    cuadro.style.transition = ('height 1s');
-    cuadro.style.height = '90%';
-}
 
 
-function manchaGrande() {
-    var mancha = document.getElementById('man');
+// function manchaGrande() {
+//     var mancha = document.getElementById('man');
 
     
-    mancha.style.transition = 'width 1s, height 1s';
-    mancha.style.width = '100%';
-    mancha.style.height = '100%';
+//     mancha.style.transition = 'width 1s, height 1s';
+//     mancha.style.width = '100%';
+//     mancha.style.height = '100%';
 
 
-    mancha.addEventListener('transitionend', function () {
-        moverMancha();
-    });
-}
+//     mancha.addEventListener('transitionend', function () {
+//         moverMancha();
+//     });
+// }
 
-function moverMancha() {
-    var mancha = document.getElementById('man');
+// function moverMancha() {
+//     var mancha = document.getElementById('man');
 
-    mancha.style.transition = 'top 1s, left 1s, width 1s, height 1s';
-    mancha.style.top = '0';
-    mancha.style.left = '0';
-    mancha.style.borderRadius = '2px 2px 30px 2px';
-    mancha.style.width = '30px';
-    mancha.style.height = '30px';
-
-
-    mancha.addEventListener('transitionend', function () {
-        esconderLogin();
-    });
-}
-
-function esconderLogin() {
-    var form = document.getElementById('f-login');
-    var titulo = document.getElementById('titulo');
-    var signUp = document.getElementById('f-signUp');
-
-    form.style.display = 'none';
-    titulo.textContent = 'Sign Up';
-    signUp.style.display = 'block';
-}
+//     mancha.style.transition = 'top 1s, left 1s, width 1s, height 1s';
+//     mancha.style.top = '0';
+//     mancha.style.left = '0';
+//     mancha.style.borderRadius = '2px 2px 30px 2px';
+//     mancha.style.width = '30px';
+//     mancha.style.height = '30px';
 
 
+//     mancha.addEventListener('transitionend', function () {
+//         esconderLogin();
+//     });
+// }
 
+// function esconderLogin() {
+//     var form = document.getElementById('f-login');
+//     var titulo = document.getElementById('titulo');
+//     var signUp = document.getElementById('f-signUp');
 
-// ---------------| BOTON VOLVER |-----------------------------------------------------------
+//     form.style.display = 'none';
+//     titulo.textContent = 'Sign Up';
+//     signUp.style.display = 'block';
+// }
 
 
 
 
-
-function manchaGrande2() {
-    var mancha = document.getElementById('man');
-
-    mancha.style.transition = 'width 1s, height 1s';
-    mancha.style.width = '100%';
-    mancha.style.height = '100%';
+// // ---------------| BOTON VOLVER |-----------------------------------------------------------
 
 
-    mancha.addEventListener('transitionend', function () {
-        volverMancha();
-    });
-}
-
-function volverMancha() {
-    var mancha = document.getElementById('man');
-
-    mancha.style.transition = 'revert-layer 2s, width 1s, height 1s';
-    mancha.style.bottom = '0';
-    mancha.style.right = '0';
-    mancha.style.inset = 'revert-layer';
-    mancha.style.borderRadius = '30px 2px 2px 2px';
-    mancha.style.width = '30px';
-    mancha.style.height = '30px';
-
-    mancha.addEventListener('transitionend', function () {
-        esconderSignUp();
-    });
-}
 
 
-function esconderSignUp() {
-    var formu = document.getElementById('f-signUp');
-    var titulo = document.getElementById('titulo');
-    var login = document.getElementById('f-login');
+// function manchaGrande2() {
+//     var mancha = document.getElementById('man');
 
-    formu.style.display = 'none';
-    titulo.textContent = 'Log-In';
-    login.style.display = 'block';
-}
+//     mancha.style.transition = 'width 1s, height 1s';
+//     mancha.style.width = '100%';
+//     mancha.style.height = '100%';
+
+
+//     mancha.addEventListener('transitionend', function () {
+//         volverMancha();
+//     });
+// }
+
+// function volverMancha() {
+//     var mancha = document.getElementById('man');
+
+//     mancha.style.transition = 'revert-layer 2s, width 1s, height 1s';
+//     mancha.style.bottom = '0';
+//     mancha.style.right = '0';
+//     mancha.style.inset = 'revert-layer';
+//     mancha.style.borderRadius = '30px 2px 2px 2px';
+//     mancha.style.width = '30px';
+//     mancha.style.height = '30px';
+
+//     mancha.addEventListener('transitionend', function () {
+//         esconderSignUp();
+//     });
+// }
+
+
+// function esconderSignUp() {
+//     var formu = document.getElementById('f-signUp');
+//     var titulo = document.getElementById('titulo');
+//     var login = document.getElementById('f-login');
+
+//     formu.style.display = 'none';
+//     titulo.textContent = 'Log-In';
+//     login.style.display = 'block';
+// }
 
 
 // ------------| VALIDACIONES |-------------------------------------------------------
